c26801a06b07c645d326b8fe3c38369ca97e0262
Rounak Barman <rounak.barman@webskitters.com>
2025-06-21 15:47:34 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const userRouter = require("./routes/userRoute");
+const blogRouter = require("./routes/blogRoute");
+const commentRouter = require("./routes/commentRoute");
+const cors = require("cors");
+const app = express();
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']  // Add 'Authorization' to allowed headers
+}));
+
+app.use(express.json());
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/blogs", blogRouter);
+app.use("/api/v1/comments", commentRouter);
+
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+
+const app = require("./app");
+const port = 5000;
+
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((config) => {
+    console.log("DB connection Successfull!!");
+  });
+
+app.listen(port, () => {
+  console.log(`App is running at ${port}...`);
+});
